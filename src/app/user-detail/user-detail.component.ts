089b544e17c43500c6dfa1fc8016c4b320c2a33d
Kai Janicek <100490095+kaix6@users.noreply.github.com>
2024-08-31 13:45:39 +0200

    user details cards

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+  styleUrl: './user-detail.component.scss',
 })
 export class UserDetailComponent {
+  userId = '';
+  user: User = new User();
+  users$: Observable<any> | undefined; // Observable für die Nutzerdaten
 
+  constructor(private route: ActivatedRoute, private firestore: Firestore) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap) => {
+      this.userId = paramMap.get('id') || '';
+      console.log('GOT ID', this.userId);
+      this.getUser();
+    });
+  }
+
+  getUser() {
+    const userDocRef = doc(this.firestore, 'users', this.userId);
+    this.users$ = docData(userDocRef);
+  
+    this.users$.subscribe((userData) => {
+      console.log('User data:', userData);
+      this.user = userData; // Weist die Daten dem user-Objekt zu
+    });
+  }
 }
